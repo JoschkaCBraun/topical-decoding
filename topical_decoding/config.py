0f357c07bc45848742ccab5149a3dfb54eecf661
Joschka <joschkacbraun@gmail.com>
2024-05-14 15:34:49 +0200

    include possibility to use OpenELM. Renamed old baseline implementation to prompt engineering and added actual baseline that just prompts the model to summarize the article

--- topical_decoding/config.py
+++ topical_decoding/config.py
@@ -4,13 +4,13 @@ This script contains all the hyperparameters that can be adapted by the user.
 """
 
 # Third-party imports
-from transformers import GenerationConfig
+from transformers import GenerationConfig, AutoTokenizer
 
-# Choose experiment between 'baseline', 'constant_shift', 'factor_scaling' and 'threshold_selection'
+# Choose experiment between 'baseline', 'prompt_engineering', 'constant_shift', 'factor_scaling' and 'threshold_selection'
 EXPERIMENT_NAME = 'baseline'
 
-# Choose model between 'gemma_2b', 'gemma_7b', 'falcon_7b', 'mistral_7b', 'llama_8b'
-MODEL_ALIAS = 'gemma_2b'
+# Choose model between 'openelm_270m', 'openelm_450m', 'openelm_1b', 'openelm_3b', 'gemma_2b', 'gemma_7b', 'falcon_7b', 'mistral_7b', 'llama_8b'
+MODEL_ALIAS = 'openelm_270m'
 
 EXPERIMENT_CONFIG = {
     'experiment_name': EXPERIMENT_NAME,
@@ -19,7 +19,7 @@ EXPERIMENT_CONFIG = {
 
 # Experiment agnostic hyperparameters
 DATASET_NAME = 'newts_train' # choose from 'newts_train', 'newts_test'
-NUM_ARTICLES = 4  # number of articles for which summaries are generated
+NUM_ARTICLES = 3  # number of articles for which summaries are generated
 BATCH_SIZE = 1  # batch size for data loader ! Code is not prepared for batch size > 1 !
 NUM_WORKERS = 0  # number of workers for generating summaries
 SHUFFLE = False  # shuffle data loader
@@ -44,7 +44,7 @@ TOPICS_CONFIG = {
 # GenerationConfig hyperparameters
 MIN_NEW_TOKENS = 80 # minimum number of tokens in the generated summary
 MAX_NEW_TOKENS = 90 # maximum number of tokens in the generated summary
-NUM_BEAMS = 4 # number of beams for beam search
+NUM_BEAMS = 1 # number of beams for beam search
 DO_SAMPLE = True # True for sampling, False for greedy decoding
 TOP_P = 0.95 # nucleus sampling parameter
 TOP_K = 50 # top-k sampling parameter
@@ -70,6 +70,10 @@ GENERATION_CONFIG = {
 
 # Experiment specific hyperparameters
 BASELINE_DICT = {
+
+}
+
+PROMPT_ENGINEERING_DICT = {
     'focus_types': ['tid1_focus', 'no_focus', 'tid2_focus'] # focus types for prompts
 }
 
@@ -86,7 +90,7 @@ THRESHOLD_SELECTION_DICT = {
     'topical_encouragement': 0.0 # factor for encouraging topic words
 }
 
-def get_generation_config(model_alias: str) -> GenerationConfig:
+def get_generation_config(model_alias: str, tokenizer: AutoTokenizer) -> GenerationConfig:
     '''Get the GenerationConfig for the model alias.
     Args:
         model_alias (str): Alias of the model.
@@ -104,10 +108,42 @@ def get_generation_config(model_alias: str) -> GenerationConfig:
                                                          top_p=TOP_P,
                                                          top_k=TOP_K,
                                                          max_length=None,
-                                                         min_length=None)
+                                                         min_length=None,
+                                                         pad_token_id=tokenizer.eos_token_id,
+                                                         use_cache=True)
     return generation_config
 
 MODEL_ALIAS_DICT= {
+     'openelm_270m': {
+        'model_name': "apple/OpenELM-270M-Instruct",
+        'tokenizer_name': "meta-llama/Llama-2-7b-hf",
+        'hf_auth_token': "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"
+    },
+    'openelm_450m': {
+        'model_name': "apple/OpenELM-450M-Instruct",
+        'tokenizer_name': "meta-llama/Llama-2-7b-hf",
+        'hf_auth_token': "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"
+    },
+    'openelm_1b': {
+        'model_name': "apple/OpenELM-1_1B-Instruct",
+        'tokenizer_name': "meta-llama/Llama-2-7b-hf",
+        'hf_auth_token': "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"
+    },
+    'openelm_3b': {
+        'model_name': "apple/OpenELM-3B-Instruct",
+        'tokenizer_name': "meta-llama/Llama-2-7b-hf",
+        'hf_auth_token': "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"
+    },   
+    'gemma_2b': {
+        'model_name': "google/gemma-2b-it",
+        'tokenizer_name': "google/gemma-2b-it",
+        'hf_auth_token': "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"
+    },
+    'gemma_7b': {
+        'model_name': 'google/gemma-7b-it',
+        'tokenizer_name': 'google/gemma-7b-it',
+        'hf_auth_token': "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"
+    },
     'mistral_7b': {
         'model_name': "mistralai/Mistral-7B-Instruct-v0.1",
         'tokenizer_name': "mistralai/Mistral-7B-Instruct-v0.1",
@@ -117,16 +153,6 @@ MODEL_ALIAS_DICT= {
         'model_name': "tiiuae/falcon-7b-instruct",
         'tokenizer_name': "tiiuae/falcon-7b-instruct",
         'hf_auth_token': None
-    },
-    'gemma_7b': {
-        'model_name': 'google/gemma-7b-it',
-        'tokenizer_name': 'google/gemma-7b-it',
-        'hf_auth_token': "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"
-    },
-    'gemma_2b': {
-        'model_name': "google/gemma-2b-it",
-        'tokenizer_name': "google/gemma-2b-it",
-        'hf_auth_token': "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"
     },
     'llama_8b': {
         'model_name': "meta-llama/Meta-Llama-3-8B-Instruct",
